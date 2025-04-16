8902304039fe7735c2bc620b41431166c76b72e0
bella podowski <143750067+bugswillbite@users.noreply.github.com>
2025-04-16 07:23:10 -0500

    Update CO.js

--- CO.js
+++ CO.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = ["coNew.png", "comain.png" ]; 
+    const images = ["coNew.png", "coMain.png" ]; 
     let currentIndex = 0;
     const imgElement = document.getElementById("slideshow");
     const nextButton = document.getElementById("nextImage");
